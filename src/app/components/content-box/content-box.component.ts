8cafe37168541e6e2d9953bcebb24fdb16bb8205
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2023-11-29 22:19:29 +0100

    sidenav is creating

--- src/app/components/content-box/content-box.component.ts
+++ src/app/components/content-box/content-box.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content-box',
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContentBoxComponent {
-
+  @Input() isFullSize: boolean = false;
 }
