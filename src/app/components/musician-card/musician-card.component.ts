d262759014bc67f963a5d943c241180f94f1150d
Lipinski <lukasz.lipinski@capgemini.com>
2023-12-16 19:29:27 +0100

    max capacity of files are fixed

--- src/app/components/musician-card/musician-card.component.ts
+++ src/app/components/musician-card/musician-card.component.ts
@@ -8,9 +8,6 @@ import { Role } from '../../pages/about-page/content/content.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MusicianCardComponent {
-  get setShowAnimation() {
-    return "show-" + this.GetImgPosition;
-  };
   private name!: string;
   get GetName() {
     return this.name;
