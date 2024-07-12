4b735d708f689a38af3a0735b8e8a76cee9e7f5d
Lukasz Lipinski <l.lipinski1993@gmail.com>
2024-07-12 14:36:43 +0200

    tests for incon=ming gigs section added

--- src/app/components/homepage-sections/incoming-gigs-section/incoming-gigs-section.component.spec.ts
+++ src/app/components/homepage-sections/incoming-gigs-section/incoming-gigs-section.component.spec.ts
@@ -1,28 +1,66 @@
-/* tslint:disable:no-unused-variable */
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { IIncomingGig, IncomingGigsSectionComponent } from "./incoming-gigs-section.component";
+import { SharedModule } from "../../../shared/shared.module";
+import { By } from "@angular/platform-browser";
+import { provideExperimentalZonelessChangeDetection } from "@angular/core";
+import { IncomingGigItemComponent } from "./incoming-gig-item/incoming-gig-item.component";
 
-import { IncomingGigsSectionComponent } from './incoming-gigs-section.component';
+const mockGigs: IIncomingGig[] = [
+  {
+    link: "test-link",
+    start: "test-start",
+    when: new Date("10-07-2024"),
+    where: {
+      address: "test-address",
+      city: "test-city",
+      club: "test-club",
+      country: "test-country",
+    },
+    who: ["testWho"]
+  }
+];
 
-describe('IncomingGigsSectionComponent', () => {
+describe("Testing Incoming Gigs Section Component", () => {
   let component: IncomingGigsSectionComponent;
   let fixture: ComponentFixture<IncomingGigsSectionComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [IncomingGigsSectionComponent]
-    })
-      .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [IncomingGigsSectionComponent],
+      imports: [SharedModule],
+      providers: [provideExperimentalZonelessChangeDetection()]
+    }).compileComponents();
     fixture = TestBed.createComponent(IncomingGigsSectionComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe("DOM tests", () => {
+    it("should create", () => {
+      expect(component).toBeTruthy();
+    });
+    it("Renders header with text 'Incoming gigs'", () => {
+      const mockTitle = "Incoming gigs";
+      const header = fixture.debugElement.query(By.css("h2")).nativeElement as HTMLHeadingElement;
+      expect(header.textContent?.trim()).toEqual(mockTitle);
+    });
+    it("Renders mocked elements", () => {
+      fixture.componentRef.setInput("SetIncomingGigs", mockGigs);
+      fixture.detectChanges();
+
+      const incomingGigsElements = fixture.debugElement.queryAll(By.directive(IncomingGigItemComponent));
+
+      expect(incomingGigsElements.length).toBe(1);
+    });
+  });
+  describe("Class tests", () => {
+    it("GetIncomingGigs return empty array by default", () => {
+      expect(component.getIncomingGigs).toEqual([]);
+    });
+    it("GetIncomingGigs return passed value", () => {
+      fixture.componentRef.setInput("SetIncomingGigs", mockGigs);
+
+      expect(component.getIncomingGigs).toEqual(mockGigs);
+      expect(component.getIncomingGigs.length).toBe(mockGigs.length);
+    });
   });
 });
