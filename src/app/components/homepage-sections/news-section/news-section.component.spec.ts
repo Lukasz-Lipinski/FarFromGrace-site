463d963d0e4c9ba862e0d61bc5a5d766f2f5729b
Lukasz Lipinski <l.lipinski1993@gmail.com>
2024-07-15 14:42:37 +0200

    news section tests added

--- src/app/components/homepage-sections/news-section/news-section.component.spec.ts
+++ src/app/components/homepage-sections/news-section/news-section.component.spec.ts
@@ -1,28 +1,56 @@
-/* tslint:disable:no-unused-variable */
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NewsSectionComponent } from "./news-section.component";
+import { SharedModule } from "../../../shared/shared.module";
+import { provideExperimentalZonelessChangeDetection } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-import { NewsSectionComponent } from './news-section.component';
+const mockNews: string[] = [
+  "news 1",
+  "news 2",
+  "news 3",
+];
 
-describe('NewsSectionComponent', () => {
+describe('Testing NewSection Component', () => {
   let component: NewsSectionComponent;
   let fixture: ComponentFixture<NewsSectionComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [NewsSectionComponent]
-    })
-      .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [SharedModule],
+      providers: [provideExperimentalZonelessChangeDetection()]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NewsSectionComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput("SetNews", mockNews);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('DOM tests', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+    it("renders header with title", () => {
+      const header = fixture.debugElement.query(By.css("h2")).nativeElement as HTMLHeadingElement;
+
+      expect(header.textContent).toContain("News");
+    });
+    it("renders 3 mocked news", () => {
+      const news = fixture.debugElement.queryAll(By.css("p"));
+
+      expect(news.length).toBe(mockNews.length);
+      for (let p of news) {
+        const element = p.nativeElement as HTMLParagraphElement;
+        expect(mockNews.includes(element.textContent?.trim()!)).toBeTrue();
+      }
+    });
+  });
+  describe('Class tests', () => {
+    it("should return news", () => {
+      expect(component.getNews).toEqual(mockNews);
+    });
+    it("returns the same number of news as mock", () => {
+      expect(component.getNews.length).toBe(mockNews.length);
+    });
   });
 });
