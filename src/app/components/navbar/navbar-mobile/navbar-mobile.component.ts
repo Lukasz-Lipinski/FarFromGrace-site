fb794907b186df5d7587b1f30dd82104df801585
Lukasz Lipinski <l.lipinski1993@gmail.com>
2024-07-24 10:46:57 +0200

    updating

--- src/app/components/navbar/navbar-mobile/navbar-mobile.component.ts
+++ src/app/components/navbar/navbar-mobile/navbar-mobile.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, Input } from '@angular/core';
 import { ILink } from "../navbar.component";
 
 @Component({
@@ -8,14 +8,12 @@ import { ILink } from "../navbar.component";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarMobileComponent {
-  @Input({
-    required: true
-  }) links!: ILink[];
+  links = input.required<ILink[]>();
 
   public get getLinks(): ILink[] {
-    return this.links;
+    return this.links();
   }
 
-  trackLink = (index: number): string => `${this.links[index].label}-${index}`;
+  trackLink = (index: number): string => `${this.links()[index].label}-${index}`;
 
 }
